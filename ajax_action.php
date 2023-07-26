21ff69a72394dca3c0051f0480b8ffedcf40dbb5
sathish19961412 <sathishfreelanc5@gmail.com>
2023-07-26 17:19:43 +0530

    Jquery and Ajax Completed to the Crud Operation

--- ajax_action.php
+++ ajax_action.php
@@ -1,28 +1,50 @@
 <?php
 
-    $con=mysqli_connect('localhost','root','','php_ajax');
-
-    $action=$_POST["action"];
-    if($action=="Insert"){
-        $name=mysqli_real_escape_string($con,$_POST["name"]);
-        $gender=mysqli_real_escape_string($con,$_POST["gender"]);
-        $contact=mysqli_real_escape_string($con,$_POST["contact"]);
-        $sql="insert into users (name,gender,contact) values ('{$name}','$gender','$contact')";
-        if($con->query($sql))
-        {
-            echo 
-            "
-            <tr>
-                <td>{$name}</td>
-                <td>{$gender}</td>
-                <td>{$contact}</td>
-                <td><a href='#' class='btn btn-primary'>Edit</a></td>
-                <td><a href='#' class='btn btn-danger'>DELETE</a></td>
-            </tr>
-            ";
-
-        }else{
-            echo false;
-        }
+  $con=mysqli_connect("localhost","root","","php_ajax");
+  
+  $action=$_POST["action"];
+  if($action=="Insert"){
+    $name=mysqli_real_escape_string($con,$_POST["name"]);
+    $gender=mysqli_real_escape_string($con,$_POST["gender"]);
+    $contact=mysqli_real_escape_string($con,$_POST["contact"]);
+    $sql="insert into users (name,gender,contact) values ('{$name}','{$gender}','{$contact}') ";
+    if($con->query($sql)){
+      $id=$con->insert_id;
+      echo "
+        <tr uid='{$id}'>
+          <td>{$name}</td>
+          <td>{$gender}</td>
+          <td>{$contact}</td>
+          <td><a href='#' class='btn btn-primary edit'>Edit</a></td>
+          <td><a href='#' class='btn btn-danger delete'>Delete</a></td>
+        </tr>";
+    }else{
+      echo false;
     }
+  }else if($action=="Update"){
+    $id=mysqli_real_escape_string($con,$_POST["id"]);
+    $name=mysqli_real_escape_string($con,$_POST["name"]);
+    $gender=mysqli_real_escape_string($con,$_POST["gender"]);
+    $contact=mysqli_real_escape_string($con,$_POST["contact"]);
+    $sql="update users SET name='{$name}',gender='{$gender}',contact='{$contact}' where id='{$id}'";
+    if($con->query($sql)){
+      echo "
+        <td>{$name}</td>
+        <td>{$gender}</td>
+        <td>{$contact}</td>
+        <td><a href='#' class='btn btn-primary edit'>Edit</a></td>
+        <td><a href='#' class='btn btn-danger delete'>Delete</a></td>";
+        
+    }else{
+      echo false;
+    }
+  }else if($action=="Delete"){
+    $id=$_POST["uid"];
+    $sql="delete from users where id='{$id}'";
+    if($con->query($sql)){
+      echo true;
+    }else{
+      echo false;
+    }
+  }
 ?>
--- index.php
+++ index.php
@@ -1,128 +1,159 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Php and Ajax Crud</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>jQuery Ajax CRUD with PHP-MySQL</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body>
-    <section>
-      <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="d-flex justify-content-center mt-5">
-                <h3>Jquery Ajax With Crud Php & Mysql Operation</h3>
-              </div>
+    <div class="modal" tabindex="-1" role="dialog" id='modal_frm'>
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">User Details</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-            <div class="col-md-12">
-                <div class="d-flex justify-content-center mt-5">
-                    <p><a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Record</a></p>
+            <div class="modal-body">
+              <form id='frm'>
+                <input type='hidden' name='action' id='action' value='Insert'>
+                <input type='hidden' name='id' id='uid' value='0'>
+                <div class='form-group'>
+                  <label>Name</label>
+                  <input type='text' name='name' id='name' required class='form-control'>
                 </div>
-                <table class="table table-bordered">
-                    <thead>
-                      <th>Name</th>
-                      <th>Gender</th>
-                      <th>Contact</th>
-                      <th>Edit</th>
-                      <th>Delete</th>
-                    </thead>
-                    <tbody id="tbody">
-                      <?php
-                           $con=mysqli_connect('localhost','root','','php_ajax');
-                           $sql="select * from users";
-                           $res=$con->query($sql);
-                           while($row=$res->fetch_assoc()){
-                              echo
-                              "
-                                 <tr>
-                                    <td>{$row['name']}</td>
-                                    <td>{$row['gender']}</td>
-                                    <td>{$row['contact']}</td>
-                                    <td><a href='#' class='btn btn-primary'>Edit</a></td>
-                                    <td><a href='#' class='btn btn-danger'>DELETE</a></td>
-                                 </tr>
-                              ";
-                           }
-                      ?>
-                    </tbody>
-                </table>
-                <!-- Add Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <div class="d-flex justify-content-center">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">User Details</h1>
-                        </div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        <form id="frm">
-                            <input type="hidden" name="action" id="action" value="Insert">
-                            <input type="hidden" name="id" id="uid" value="0">
-                            <div class="mb-3 form-group">
-                                <label for="exampleFormControlInput1" class="form-label">Name</label>
-                                <input type="text" class="form-control" name="name" id="name" placeholder="Name">
-                            </div>
-                            <div class="mb-3 form-group">
-                              <select name="gender" id="gender" required class="form-control">
-                                    <option value=''>Select</option>
-                                    <option value='Male'>Male</option>
-                                    <option value='Female'>Female</option>
-                                    <option value='Others'>others</option>
-                              </select>
-                            </div>
-                            <div class="mb-3 form-group">
-                                <label for="exampleFormControlInput1" class="form-label">Contact</label>
-                                <input type="text" class="form-control" name="contact" id="contact" placeholder="Name">
-                            </div>
-                            <input type="submit" value="Submit" class="btn btn-success">
-                        </form>
-                      </div>
-                    </div>
-                  </div>
+                <div class='form-group'>
+                  <label>Gender</label>
+                  <select name='gender' id='gender' required class='form-control'>
+                    <option value=''>Select</option>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                    <option value='Others'>Others</option>
+                  </select>
                 </div>
+                <div class='form-group'>
+                  <label>Contact</label>
+                  <input type='text' name='contact' id='contact' required class='form-control'>
+                </div>
+                <input type='submit' value='Submit' class='btn btn-success'>
+              </form>
             </div>
           </div>
       </div>
-    </secion>
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </div>
+
+    <div class='container mt-5'>
+        <p class='text-right'><a href='#' class='btn btn-success' id='add_record'>Add Record</a></p>
+      <table class='table table-bordered'>
+      <thead>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Contact</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </thead>
+      <tbody id='tbody'>
+        <?php 
+          $con=mysqli_connect("localhost","root","","php_ajax");
+          $sql="select * from users";
+          $res=$con->query($sql);
+          while($row=$res->fetch_assoc()){
+            echo "
+              <tr uid='{$row["id"]}'>
+                <td>{$row["name"]}</td>
+                <td>{$row["gender"]}</td>
+                <td>{$row["contact"]}</td>
+                <td><a href='#' class='btn btn-primary' id='edit'>Edit</a></td>
+                <td><a href='#' class='btn btn-danger' id='delete'>Delete</a></td>
+              </tr>
+            ";
+          }
+        ?>
+      </tbody>
+      </table>
+    </div>
     <script>
-        $(document).ready(function(){
-            $("#frm").submit(function(event){
-               event.preventDefault();
-               $.ajax({
-                    url:"ajax_action.php",
-                    type:"post",
-                    data:$("#frm").serialize(),
-                    beforeSend:function(){
-
-                          $("#frm").find("input[type='submit']").val('Loading...');
-
-                    },
-                    success:function(res){
-
-                      if(res)
-                      {
-                          $("#tbody").append(res);
-                      }else{
-                         alert('Failed try Again')
-                      }
-                      $("#frm").find("input[type='submit']").val('Submit');
-                      clear_input();
-                      $("#exampleModal").modal('hide');
-                    }
-               });
-
-               function clear_input(){
-                  $('#frm').find(".form-control").val("");
-                  $("#action").val("Insert");
-                  $("#uid").val("0");
-               }
-            });
+      $(document).ready(function(){
+        var current_row=null;
+        $("#add_record").click(function(){
+          $("#modal_frm").modal();
         });
+        
+        $("#frm").submit(function(event){
+          event.preventDefault();
+          $.ajax({
+            url:"ajax_action.php",
+            type:"post",
+            data:$("#frm").serialize(),
+            beforeSend:function(){
+              $("#frm").find("input[type='submit']").val('Loading...');
+            },
+            success:function(res){
+              if(res){
+                if($("#uid").val()=="0"){
+                  $("#tbody").append(res);
+                }else{
+                  $(current_row).html(res);
+                }
+              }else{
+                alert("Failed Try Again");
+              }
+              $("#frm").find("input[type='submit']").val('Submit');
+              clear_input();
+              $("#modal_frm").modal('hide');
+            }
+          });
+        });
+        
+        $("body").on("click","#edit",function(event){
+          event.preventDefault();
+          current_row=$(this).closest("tr");
+          $("#modal_frm").modal();
+          var id=$(this).closest("tr").attr("uid");
+          var name=$(this).closest("tr").find("td:eq(0)").text();
+          var gender=$(this).closest("tr").find("td:eq(1)").text();
+          var contact=$(this).closest("tr").find("td:eq(2)").text();
+          
+          $("#action").val("Update");
+          $("#uid").val(id);
+          $("#name").val(name);
+          $("#gender").val(gender);
+          $("#contact").val(contact);
+        });
+        
+        $("body").on("click","#delete",function(event){
+          event.preventDefault();
+          var id=$(this).closest("tr").attr("uid");
+          var cls=$(this);
+          if(confirm("Are You Sure")){
+            $.ajax({
+              url:"ajax_action.php",
+              type:"post",
+              data:{uid:id,action:'Delete'},
+              beforeSend:function(){
+                $(cls).text("Loading...");
+              },
+              success:function(res){
+                if(res){
+                  $(cls).closest("tr").remove();
+                }else{
+                  alert("Failed TryAgain");
+                  $(cls).text("Try Again");
+                }
+              }
+            });
+          }
+        });
+        
+        function clear_input(){
+          $("#frm").find(".form-control").val("");
+          $("#action").val("Insert");
+          $("#uid").val("0");
+        }
+      });
     </script>
   </body>
 </html>
